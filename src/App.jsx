42191762c4c560990e0bb608d73f06ab63da90ae
Juan Perez <142500323+2Vka0@users.noreply.github.com>
2024-10-22 10:07:52 -0500

    Se sube el proyecto al repositorio

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+
+
+import { Rutas } from './Routes/Routes'
+
+import { BrowserRouter } from "react-router-dom";
+
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.min.js"
+
+function App() {
+  return (
+    <>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente eius earum, natus aliquam perferendis illum accusantium non animi sit nostrum dolores minima excepturi inventore pariatur quaerat unde. Minus, dicta quas.</p>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/FormularioRegistro/FormularioRegistro.jsx
@@ -0,0 +1,14 @@
+
+
+import React from 'react'
+
+function FormularioRegistro() {
+  return (
+    
+    <>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum a at nobis iusto tempora eius ipsam, architecto voluptatem nihil perspiciatis saepe laudantium soluta voluptatum magnam porro, non consequatur sit nesciunt.</p>
+    </>
+  )
+}
+
+export default FormularioRegistro
